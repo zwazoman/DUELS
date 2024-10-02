78081ac5d8093269cabc9350c8044b6a62da6e63
zwazoman <141416442+zwazoman@users.noreply.github.com>
2024-10-02 14:06:04 +0200

    ça avance

---
+++ Assets/_Scripts/Nestor/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float _speed = 5f;
+    [SerializeField] float _jumpForce = 5f;
+
+    bool _isGrounded = false;
+    float _horizontal;
+    Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, 0.5f))
+        {
+            _isGrounded = true;
+        }
+        else
+        {
+            _isGrounded = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector3(_horizontal * _speed,_rb.velocity.y, _rb.velocity.z);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        _horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (!_isGrounded) return;
+            _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        }    
+    }
+}
