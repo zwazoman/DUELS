b0d27bd021ad94fa72c6faa40eee594a65b4d582
Elouann <elouann.jouenne@e-artsup.net>
2024-10-04 14:31:28 +0200

    Font kaph + text modifiers (et d'autres trucs)

--- Assets/_Scripts/Elouann/GameTimer.cs
+++ Assets/_Scripts/Elouann/GameTimer.cs
@@ -4,12 +4,15 @@ using TMPro;
 using System;
 using UnityEngine;
 using DG.Tweening;
+using Unity.VisualScripting;
 
 public class GameTimer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _gameTimer;
 
     private float _timer;
+
+    bool canScale = true;
 
     private void Start()
     {
@@ -19,10 +22,11 @@ public class GameTimer : MonoBehaviour
 
     public IEnumerator StartTimer()
     {
-        StartCoroutine(ScaleTimer());
+        //StartCoroutine(ScaleTimer());
         while (true)
         {
             yield return new WaitForSeconds(0.01f);
+            //if(Mathf.Round(_timer) % 10 == 0) StartCoroutine(Scale());
             _timer += 0.01f;
             _gameTimer.text = String.Format("{0:0.00}", _timer);
         }
@@ -32,11 +36,24 @@ public class GameTimer : MonoBehaviour
     {
         while (true)
         {
-        yield return new WaitForSeconds(10.01f);
-        print("OY");
-        _gameTimer.transform.DOScale(2, 0.2f).SetEase(Ease.InOutElastic);
+        yield return new WaitForSeconds(10.75f);
+        print("OY" + _timer.ToString());
+        _gameTimer.transform.DOScale(2, 0.2f).SetEase(Ease.OutBack);
         yield return new WaitForSeconds(0.2f);
         _gameTimer.transform.DOScale(1, 0.4f);
+        }
+    }
+
+    private IEnumerator Scale()
+    {
+        if (canScale)
+        {
+            canScale = false;
+            print(_timer);
+            _gameTimer.transform.DOScale(2, 0.2f).SetEase(Ease.OutBack);
+            yield return new WaitForSeconds(0.2f);
+            _gameTimer.transform.DOScale(1, 0.4f);
+            canScale = true;
         }
     }
 }
